e2551163ed909910717474444c076ab57e518ff6
Akira Vitor de Lima Yamazaki <akira.vitor@intellissis.com.br>
2026-03-25 11:39:41 -0300

    Atividade 3

--- ativ1.sql
+++ ativ1.sql
@@ -1,17 +1,16 @@
 -- Atividade 1
 CREATE TABLE
     modelo (
-        id INT NOT NULL,
-        nome VARCHAR(50) NOT NULL,
+        id INT NOT NULL CHECK (id > 0),
+        nome VARCHAR(50) NOT NULL UNIQUE CHECK (nome <> ''),
         descricao VARCHAR(100),
-        PRIMARY KEY (id),
-        UNIQUE (nome)
+        PRIMARY KEY (id)
     );
 
 CREATE TABLE
     item (
-        num_serie INT NOT NULL,
-        valor_aquisicao INT NOT NULL CHECK (valor_aquisicao > 0),
+        num_serie INT NOT NULL CHECK (num_serie > 0),
+        valor_aquisicao DECIMAL NOT NULL CHECK (valor_aquisicao > 0),
         modelo_id INT NOT NULL,
         PRIMARY KEY (num_serie),
         FOREIGN KEY (modelo_id) REFERENCES modelo (id)
@@ -19,23 +18,22 @@ CREATE TABLE
 
 CREATE TABLE
     cliente (
-        cpf CHAR(11) NOT NULL,
+        cpf CHAR(11) NOT NULL CHECK (cpf NOT LIKE '%[^0-9]%' AND LEN(cpf) = 11),
         nome VARCHAR(50) NOT NULL CHECK (nome <> ''),
-        data_nasc DATE,
-        genero CHAR(1), -- M, F, O (outro)
+        data_nasc DATE NULL,
+        genero CHAR(1) NULL CHECK (genero IN ('M','F','O') OR genero IS NULL),
         PRIMARY KEY (cpf)
     );
 
 CREATE TABLE
     venda (
-        -- id INT NOT NULL,
-        data DATETIME NOT NULL,
-        valor INT NOT NULL CHECK (valor > 0),
-        cpf_comprador CHAR(11) NOT NULL,
+        data DATETIME NOT NULL CHECK (data <= GETDATE()),
+        valor DECIMAL NOT NULL CHECK (valor > 0),
+        cpf_comprador CHAR(11) NOT NULL CHECK (cpf_comprador NOT LIKE '%[^0-9]%' AND LEN(cpf_comprador) = 11),
         id_item INT NOT NULL,
         FOREIGN KEY (cpf_comprador) REFERENCES cliente (cpf),
         FOREIGN KEY (id_item) REFERENCES item (num_serie),
-        PRIMARY KEY (cpf_comprador, id_item, data)
+        PRIMARY KEY (id_item)
     );
 
 INSERT INTO
@@ -64,12 +62,7 @@ INSERT INTO
 VALUES
     ('12345678901', 'João Silva', '1990-01-01', 'M'),
     ('23456789012', 'Maria Santos', '1985-05-15', 'F'),
-    (
-        '34567890123',
-        'Pedro Oliveira',
-        '1992-10-20',
-        'M'
-    ),
+    ('34567890123','Pedro Oliveira','1992-10-20','M'),
     ('45678901234', 'Ana Costa', '1988-03-10', 'O');
 
 INSERT INTO
@@ -101,35 +94,3 @@ SELECT
 FROM
     venda;
 
--- Ativ 2
-CREATE TABLE
-    avaliacao (
-        cpf_cliente CHAR(11) NOT NULL,
-        id_modelo INT NOT NULL,
-        nota INT NOT NULL CHECK (
-            nota >= 0
-            AND nota <= 5
-        ),
-        comentario VARCHAR(255),
-        PRIMARY KEY (cpf_cliente, id_modelo),
-        FOREIGN KEY (cpf_cliente) REFERENCES cliente (cpf),
-        FOREIGN KEY (id_modelo) REFERENCES modelo (id)
-    );
-
-INSERT INTO
-    avaliacao (cpf_cliente, id_modelo, nota, comentario)
-VALUES
-    ('23456789012',1,0,'Preferia uma bomba escondida'),
-    ('12345678901', 1, 1, 'Horrível'),
-    ('34567890123', 1, 2, 'Ok.'),
-    ('45678901234', 1, 3, 'Satisfeito.'),
-    ('12345678901', 2, 4, 'Show de bola'),
-    ('23456789012', 2, 5, 'Perfeito');
-
-SELECT
-    id_modelo as "ID do modelo",
-    AVG(nota) AS "Nota média"
-FROM
-    avaliacao
-GROUP BY
-    id_modelo;
---
+++ ativ3.sql
@@ -0,0 +1,27 @@
+CREATE MATERIALIZED VIEW lucro_modelo AS
+SELECT
+    M.nome AS modelo,
+    COALESCE(SUM(V.valor - I.valor_aquisicao), 0) AS lucro_acumulado
+FROM
+    modelo M
+LEFT JOIN item I ON I.modelo_id = M.id
+LEFT JOIN venda V ON V.id_item = I.num_serie
+GROUP BY
+    M.nome
+ORDER BY
+    lucro_acumulado DESC;
+
+SELECT *
+FROM lucro_modelo;
+
+-- v2
+CREATE TABLE lucro AS
+SELECT
+    VENDA.id_item,
+    (VENDA.valor - ITEM.valor_aquisicao) AS lucro_item
+FROM
+    venda VENDA
+JOIN item ITEM ON VENDA.id_item  = ITEM.num_serie;
+
+SELECT *
+FROM lucro
